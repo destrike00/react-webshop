c1053ebc91d44d4167b8a9495bead863b02e1193
destrike00 <k.aron147@gmail.com>
2023-06-05 14:14:47 +0200

    getting rid of npm lock, added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+
+const Shop = () => {
+    return <h1>Shop page</h1>;
+};
 const App = () => {
-    return <Directory />;
+    return (
+        <Routes>
+            <Route path="/home" element={<Home />}>
+                <Route path="shop" element={<Shop />} />
+            </Route>
+        </Routes>
+    );
 };
 export default App;
